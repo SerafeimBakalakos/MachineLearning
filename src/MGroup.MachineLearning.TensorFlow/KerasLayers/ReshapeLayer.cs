a0d03d71f0f1f9a9bf292749dd9614fe2f16b583
SerafeimBakalakos <serafeim.bakalakos@outlook.com>
2023-10-04 12:36:18 +0300

    Reshape layer and first attempt at 1D transpose convolution

--- src/MGroup.MachineLearning.TensorFlow/KerasLayers/ReshapeLayer.cs
+++ src/MGroup.MachineLearning.TensorFlow/KerasLayers/ReshapeLayer.cs
@@ -1,21 +1,17 @@
 namespace MGroup.MachineLearning.TensorFlow.KerasLayers
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Text;
-
 	using Tensorflow;
 	using Tensorflow.Keras.ArgsDefinition;
 	using Tensorflow.Keras.Layers;
 
 	public class ReshapeLayer : INetworkLayer
 	{
-		public int[] TargetShape { get; }
-
 		public ReshapeLayer(int[] targetShape)
 		{
 			this.TargetShape = targetShape;
 		}
+
+		public int[] TargetShape { get; }
 
 		public Tensors BuildLayer(Tensors output) => new Reshape(new ReshapeArgs()
 		{
